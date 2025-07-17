0447755cd535125e1d2b84bda1f775224d38c4dc
Captain Obvious <obviouspain@gmail.com>
2025-07-17 16:32:27 +0200

    test

---
+++ src/main.rs
@@ -0,0 +1,26 @@
+mod handlers;
+mod models;
+
+use actix_web::{App, HttpServer, web};
+use mongodb::{Client, Database};
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    let mongo_uri = std::env::var("MONGODB_URI")
+        .unwrap_or_else(|_| "mongodb://localhost:27017".into());
+    let client: Client = Client::with_uri_str(&mongo_uri)
+        .await
+        .expect("failed to connect to MongoDB");
+    let db: Database = client.database("unimsg");
+
+    println!("Server running at http://127.0.0.1:8080");
+    HttpServer::new(move || {
+        App::new()
+            .app_data(web::Data::new(db.clone()))
+            .service(handlers::ingest)
+            .service(handlers::get_conversation)
+    })
+        .bind(("127.0.0.1", 8080))?
+        .run()
+        .await
+}
